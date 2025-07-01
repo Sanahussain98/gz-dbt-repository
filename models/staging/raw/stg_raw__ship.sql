bac6d5852615367b5d189632fa7f160410a57bde
Sana Hussain <sanahussain526@gmail.com>
2025-07-01 20:16:02 +0000

    updated

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -12,7 +12,7 @@ renamed as (
         orders_id,
         shipping_fee,
         shipping_fee_1,
-        logcost,
+        logcost AS log_cost,
         cast(ship_cost as FLOAT64) as ship_cost,
 
     from source
